249138e3f71d6fe5d4e7d041dbe6090ee0388f69
Leonardo Emerson <70107262+leonardoemerson@users.noreply.github.com>
2022-02-13 21:37:42 -0300

    Adiciona validação de email e telefone.

--- Models/ong.cs
+++ Models/ong.cs
@@ -9,6 +9,15 @@ namespace Inclusion.Models
     [Column(Order = 1)]
 
     public int id { get; set; }
+    
+    [StringLength(60, MinimumLength = 7)]
+    [Required]
+    public string? email { get; set; }
+
+    [RegularExpression(@"^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$")]
+    [Required]
+    [StringLength(20)]
+    public string? telefone { get; set; }
 
     [Required]
     [StringLength(50, MinimumLength = 5, ErrorMessage = "O nome da ONG deve conter entre 5 e 50 caracteres.")]
