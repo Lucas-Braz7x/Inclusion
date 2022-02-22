ae4834679bec2342c9812cc2d5330868ed95c1b3
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-02-21 22:46:52 -0300

    Commit inicial

---
+++ src/main/java/com/api/inclusion/ApiInclusionApplication.java
@@ -0,0 +1,13 @@
+package com.api.inclusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInclusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInclusionApplication.class, args);
+	}
+
+}
