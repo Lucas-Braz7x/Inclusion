173e784a2d707339726da90a917762d85d5cb74f
Leonardo Emerson <70107262+leonardoemerson@users.noreply.github.com>
2022-02-26 12:12:03 -0300

    Adding contact's route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Home } from './Pages/Home'
 import { Container } from './Components/Container';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Equipamentos } from './Pages/Equipamentos';
+import { Contatos } from './Pages/Contatos';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<Equipamentos />} path="/equipamentos" />
+          <Route element={<Contatos />} path="/contatos" />
         </Routes>
         <Footer />
       </BrowserRouter>
