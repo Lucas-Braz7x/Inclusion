8c542516e5ff6d0fd0ac78847dbb67ee8cd778f2
crisraele <86687969+crisraele@users.noreply.github.com>
2022-03-07 15:34:32 -0300

    Update Equipamentos.java

--- Java/src/main/java/com/api/inclusion/model/Equipamentos.java
+++ Java/src/main/java/com/api/inclusion/model/Equipamentos.java
@@ -1,5 +1,33 @@
 package com.api.inclusion.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Equipamentos {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+	private String nomeEquipamento;
+	
+	@Column(nullable = false, length = 50)
+	private String descricao;
+
+	@Column(nullable = false, length = 50)
+	private String doadorId;
 
 }
