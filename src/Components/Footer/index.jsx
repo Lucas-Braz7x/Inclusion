41fb3a44b7207c4ef5737be5eb4833d0748d9c3f
Leonardo Emerson <70107262+leonardoemerson@users.noreply.github.com>
2022-03-13 12:14:28 -0300

    Atualiza footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -7,23 +7,32 @@ export const Footer = () => {
     <footer id="myFooter">
       <div className="container">
         <ul>
+
           <li><a >Sobre</a></li>
           <li><a >Time</a></li>
           <li><a >Equipamentos</a></li>
           <li><a >Ong Amigas</a></li>
           <li><a >Doadores</a></li>
           <li><a href="/Contatos">Contato</a></li>
+          
         </ul>
         <p className="footer-copyright">© 2021 Copyright - Squad 42</p>
       </div>
+
       <div className="footer-social">
-        <a href="#" className="social-icons"><img className="fa fa-facebook" 
-            src="~/assets/facebook.svg" alt="Ícone do facebook"/></a>
-        <a href="#" className="social-icons"><img className="fa fa-instagram" 
-            src="~/assets/instagram.svg" alt="Ícone do instagram"/></a>
-        <a href="#" className="social-icons"><img className="fa fa-twitter" 
-            src="~/assets/twitter.svg" alt="Íconde do twitter"/></a>
+
+        <a href="#"><img className='social-icons'
+              src={require('../../assets/facebook.svg').default} alt="Ícone do facebook"/>
+        </a>
+        <a href="#"><img className='social-icons' 
+              src={require('../../assets/instagram.svg').default} alt="Ícone do instagram"/>
+        </a>
+        <a href="#"><img className='social-icons' 
+              src={require('../../assets/twitter.svg').default} alt="Íconde do twitter"/>
+        </a>
+
       </div>
+
     </footer>
   )
 }
