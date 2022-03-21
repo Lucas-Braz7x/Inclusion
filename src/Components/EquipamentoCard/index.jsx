f52d59891384d158d6055da2000520457044f9ef
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-03-21 19:12:32 -0300

    Estiliza modal do equipamentoCard

--- src/Components/EquipamentoCard/index.jsx
+++ src/Components/EquipamentoCard/index.jsx
@@ -3,14 +3,20 @@ import './styles.scss';
 import * as P from 'prop-types';
 import img from '../../assets/sem-imagem.jpg';
 import { FiLink } from 'react-icons/fi';
+import { FiTrash2 } from 'react-icons/fi';
+import { BiEditAlt } from 'react-icons/bi';
 import { MdAdd } from 'react-icons/md';
 import { Modal } from '../UI/Modal';
-
-
+//import { registrarToken } from '../../Service';
+import { useJwt } from "react-jwt";
+//import { mostrarMensagem } from '..';
+//import { useNavigate } from 'react-router-dom';
 
 export const EquipamentoCard = ({ filtro, equipamento }) => {
   const [modal, setModal] = useState(false);
-  console.log(equipamento)
+
+  const { decodedToken } = useJwt(localStorage.getItem("USUARIO_LOGADO"));
+
 
   return (
     <>
@@ -40,19 +46,32 @@ export const EquipamentoCard = ({ filtro, equipamento }) => {
           <div className='modalContainer'>
             <div className='modalContent'>
               <div className='modalImage'>
-                {equipamento.imageUrl.length > 15 &&
-                  <img src={equipamento.imageUrl} alt={equipamento.imageUrl} />
-                }
+                <img src={equipamento.imageUrl == "sem imagem" ? img : equipamento.imageUrl}
+                  alt={equipamento.imageUrl} />
               </div>
               <div>
-                <h1>{equipamento.nomeEquipamento}</h1>
-                <p>{equipamento.descricao}</p>
-                <p>{equipamento.tipoDeficiencia}</p>
-                <p>{equipamento.doador.nomeDoador}</p>
-                <p>{equipamento.doador.telefone}</p>
-                <p>{equipamento.doador.endereco}</p>
-                <p>{equipamento.doador.cep}</p>
-                <p>{equipamento.doador.estado}</p>
+
+                <div className='modalEquipamento'>
+                  <h1>{equipamento.nomeEquipamento}</h1>
+                  <p>Descrição: {equipamento.descricao}</p>
+                  <p>Tipo de deficiência: {equipamento.tipoDeficiencia}</p>
+                </div>
+
+                <div className='modalDoador'>
+                  <p>Nome: {equipamento.doador.nomeDoador} | {equipamento.doador.email}</p>
+                  <p>Contato: {equipamento.doador.telefone}</p>
+                  <p>
+                    Endereço: {equipamento.doador.endereco}-{equipamento.doador.estado}</p>
+                </div>
+
+                <div className='modalIcons'>
+                  {decodedToken.id == equipamento.doador.id && (
+                    <>
+                      <FiTrash2 />
+                      <BiEditAlt />
+                    </>
+                  )}
+                </div>
               </div>
             </div>
           </div>
