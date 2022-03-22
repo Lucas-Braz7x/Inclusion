d4392b724fb019180c47bbffb84bacea96acf0f3
Leonardo Emerson <70107262+leonardoemerson@users.noreply.github.com>
2022-03-22 13:47:02 -0300

    Update index.jsx

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,62 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../Login/assets/inclusion-logo.png';
-import { Link } from 'react-router-dom';
-//import { MdReorder } from "react-icons/md";
-
+import {Link} from 'react-router-dom';
 
 export const Header = () => {
-  return (
-    <header id="header" className="fixed-top">
-      <div className="container d-flex align-items-center">
 
-        <div className="login-form-title">
-          <img src={logo} alt="inclusion" />
+const [classOn, setClassOn] = useState(false);
+      
+  return (
+    <header>
+    <div className="container">
+      <div>
+      <Link to="/"><img className="logo-cyan" src={logo} alt="logo Cyan"></img></Link>
+      </div>
+      
+
+      <div className = { classOn ? 'menu-section on' : 'menu-section'} onClick={() => setClassOn(!classOn)}>
+           <div className="menu-toggle">
+              <div className="one"></div>
+              <div className="two"></div>
+              <div className="three"></div>
+           </div>
+
+           <nav id="navbar" className="navbar navbar-expand-lg">
+            <ul className="navbar-nav item-list">
+              <li>
+                <Link to="/" style={{"color": "white"}}>Página Inicial</Link>
+              </li>
+              <li>
+                <Link to="/Equipamentos" style={{"color": "white"}}>Equipamentos</Link>
+              </li>
+              <li>
+                <Link to="/Time" style={{"color": "white"}}>Time</Link>
+              </li>
+              <li>
+                <Link to="/Contatos" style={{"color": "white"}}>Contatos</Link>
+              </li>
+              <li>
+                <Link to='/cadastro' className="getstarted scrollto">Doação</Link>  
+              </li>
+              <li>
+                <Link to="/cadastro/ong" className="getstarted scrollto" style={{"color": "white"}}>Ongs</Link>  
+              </li>
+            </ul>
+          </nav>
+
+          </div>
         </div>
 
-        <nav id="navbar" className="navbar navbar-expand-lg">
+      </header>
 
-          <button className="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
-            <span className="navbar-toggle-icon"></span>
-          </button>
-
-          <ul className="navbar-nav item-list">
-
-            <li className="nav-item">
-              <a className="nav-link scrollto" href="/">Página
-                Inicial</a>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link scrollto" href="/Equipamentos">Equipamentos</a>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link scrollto" href="/Time">Time</a>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link scrollto" href="/Contatos">Contatos</a>
-            </li>
-
-            <li className="nav-item">
-              <a className="getstarted scrollto" href='/cadastro'>Fazer Doação</a>
-            </li>
-
-            <li className="nav-item">
-              <Link className="getstarted scrollto" to="/cadastro/ong" >Ongs</Link>
-
-            </li>
-
-          </ul>
-
-          <i className="bi bi-list mobile-nav-toggle"></i>
-
-        </nav>
-
-      </div>
-
-    </header>
-  )
-}
+      )
+    }
+    
